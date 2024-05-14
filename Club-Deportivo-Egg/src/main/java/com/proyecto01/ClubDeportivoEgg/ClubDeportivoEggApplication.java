b6275ba3fd3ea24d28d84f5e598bcde3dea88f1b
Leandro Muro <leandro_mu96@hotmail.com>
2024-05-13 23:20:00 -0300

    Creacion del Back Spring

---
+++ Club-Deportivo-Egg/src/main/java/com/proyecto01/ClubDeportivoEgg/ClubDeportivoEggApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto01.ClubDeportivoEgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubDeportivoEggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubDeportivoEggApplication.class, args);
+	}
+
+}
---
+++ Club-Deportivo-Egg/src/test/java/com/proyecto01/ClubDeportivoEgg/ClubDeportivoEggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto01.ClubDeportivoEgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubDeportivoEggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
