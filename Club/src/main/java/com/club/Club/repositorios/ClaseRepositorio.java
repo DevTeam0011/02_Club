90813c24c4dee41bf0d7726c23e2c379bbee62d3
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-05-17 11:20:38 -0300

    clase repositorio completo

--- Club/src/main/java/com/club/Club/repositorios/ClaseRepositorio.java
+++ Club/src/main/java/com/club/Club/repositorios/ClaseRepositorio.java
@@ -1,1 +1,10 @@
 package com.club.Club.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.club.Club.entidades.Clase;
+@Repository
+public interface ClaseRepositorio  extends JpaRepository<Clase,Long>{
+    
+}
