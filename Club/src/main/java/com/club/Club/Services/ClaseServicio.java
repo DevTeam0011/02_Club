b50bc3cc0896669a255c638785f3c42f32b435bb
EzequielMovia <136653654+EzequielMovia@users.noreply.github.com>
2024-05-23 12:06:44 -0300

    servicio terminado

--- Club/src/main/java/com/club/Club/Services/ClaseServicio.java
+++ Club/src/main/java/com/club/Club/Services/ClaseServicio.java
@@ -1,25 +1,111 @@
 package com.club.Club.Services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.club.Club.DTO.ClaseCreateDTO;
+import com.club.Club.DTO.SocioCreateDTO;
+import com.club.Club.Entities.Clase;
+import com.club.Club.Entities.Entrenador;
+import com.club.Club.Entities.Socio;
 import com.club.Club.Others.metodosUtiles;
 import com.club.Club.Repositories.ClaseRepositorio;
+import com.club.Club.Repositories.EntrenadorRepositorio;
 
 @Service
 public class ClaseServicio {
     @Autowired
     ClaseRepositorio claseRepositorio;
+    @Autowired
+    EntrenadorRepositorio entrenadorRepositorio;
 
     //Crear una clase
     @Transactional
-    public void crearClase(ClaseCreateDTO createDTO) throws Exception{
+    public void crearClase(ClaseCreateDTO CDTO) throws Exception{
         metodosUtiles.validateFieldsAreNotEmptyOrNull(
-                new String[] { "nombre_clase", "descripcion", "horario", "entrenador" }, createDTO.getNombre_clase(), createDTO.getDescripcion(), createDTO.getHorario(), createDTO.getHorario(), createDTO.getEntrenador());
+                new String[] { "nombre", "horario", "turno","entrenador_id" }, CDTO.getNombre(), 
+                CDTO.getHorario(), CDTO.getTurno(), CDTO.getEntrenador_id());
+
+                Clase newclase = new Clase();
+                 newclase.setNombre(CDTO.getNombre());
+                 newclase.setHorario(CDTO.getHorario());
+                 newclase.setTurno(CDTO.getTurno());
+                 
+
+                 Entrenador entrenador = entrenadorRepositorio.findById(CDTO.getEntrenador_id().getId())
+                .orElseThrow(() -> new Exception("No se encontro entrenador con ese id"));
+
+                newclase.setEntrenador_id(entrenador);
+                claseRepositorio.save(newclase);
+                 
+
 
         
 
     }
+    @Transactional(readOnly = true)
+    public List<Clase> listarSocio() {
+        List<Clase> clase = new ArrayList();
+        clase = claseRepositorio.findAll();
+        return clase;
+    }
+
+    // TRAER SOCIOS POR ID-----------------------------------------------
+    public Clase getOne(ClaseCreateDTO CDTO) {
+        Optional<Clase> respuesta =claseRepositorio.findById(CDTO.getId());
+        if (respuesta.isPresent()) {
+            Clase newclase = respuesta.get();
+            return newclase;
+        }
+        return null;
+    }
+// MODIFICAR SOCIO-----------------------------------------------
+
+@Transactional
+public void modificarClase(ClaseCreateDTO CDTO) throws Exception {
+
+    // VALIDAR ACTIVIVDAD
+    metodosUtiles.validateFieldsAreNotEmptyOrNull(
+            new String[] { "nombre", "horario", "turno","entrenador_id" }, CDTO.getNombre(),
+            CDTO.getNombre(),
+            CDTO.getHorario(), CDTO.getTurno(), CDTO.getEntrenador_id());
+
+    Optional<Clase> clase =  claseRepositorio.findById(CDTO.getId()); //claseRepositorio.findById(CDTO.getId());
+
+    if (clase.isPresent()) {
+        Clase respuesta = clase.get();
+        respuesta.setNombre(CDTO.getNombre());
+        respuesta.setHorario(CDTO.getHorario());
+        respuesta.setTurno(CDTO.getTurno());
+        respuesta.setEntrenador_id((CDTO.getEntrenador_id()));
+        
+        claseRepositorio.save(respuesta);
+    }
+    
+
+}
+@Transactional
+public void modificarEstadoClase(ClaseCreateDTO CDTO) {
+
+    Optional<Clase> clase = claseRepositorio.findById(CDTO.getId());
+
+    if (clase.isPresent()) {
+        Clase newsocio = clase.get();
+
+        
+          /* if(newclase.isAlta()){
+            newsocio.setAlta(false);
+          }else {
+            newsocio.setAlta(true);
+          }
+          } */
+         
+    }
+    
+}
 }
