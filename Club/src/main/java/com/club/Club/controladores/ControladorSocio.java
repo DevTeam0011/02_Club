820e6c344c76a0f6c5fb6c926f47117eb092104e
EzequielMovia <136653654+EzequielMovia@users.noreply.github.com>
2024-05-27 22:46:49 -0300

    creacion de de controlador socio

---
+++ Club/src/main/java/com/club/Club/controladores/ControladorSocio.java
@@ -0,0 +1,62 @@
+package com.club.Club.controladores;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.club.Club.DTO.SocioCreateDTO;
+import com.club.Club.Entities.Socio;
+import com.club.Club.Services.SocioServicio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/api/socio")
+public class ControladorSocio {
+
+      @Autowired
+    SocioServicio socioServicio;
+
+    // CREAR CLASE
+    @PostMapping("")
+    public void crearClase(@RequestBody SocioCreateDTO SDTO) throws Exception {
+
+        socioServicio.CrearEntrenador(SDTO);
+    }
+
+   // LISTAR CLASE
+    @GetMapping("")
+    public List<Socio> listarClases() {
+        List<Socio> list = socioServicio.listarSocio();
+
+        return list;
+    }
+
+  // TRAER ACTIVIDAD POR ID
+  @GetMapping("/socio_id/")
+  public Socio traersocioId(@RequestBody SocioCreateDTO SDTO) {
+      return socioServicio.getOne(SDTO);
+
+  }
+
+  // MODIFICAR ESTADO (DAR ALTA/DAR BAJA)
+  @PutMapping("/modificar_estado")
+  public void modificarEstadoActividad(@RequestBody SocioCreateDTO SDTO){
+    socioServicio.modificarEstadoSocio(SDTO);
+
+  }
+
+}
+ 
+
+   
+
+    
+
+
+    
+
