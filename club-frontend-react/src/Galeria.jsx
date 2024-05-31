1046358ddf741d6f9368edec19fbc5454566659a
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-05-31 13:20:17 -0300

    creación de efecto en galería

--- club-frontend-react/src/Galeria.jsx
+++ club-frontend-react/src/Galeria.jsx
@@ -3,18 +3,23 @@ import imgrugby2 from '../public/imagenes/kids-56952_1280.jpg'
 import imgpaddle3 from '../public/imagenes/paddle-tennis-6612340_1280.jpg'
 import imgchicos4 from '../public/imagenes/hockey-3386656_1280.jpg'
 
+const ContenedorDeImg = ({srcImg, alt}) => {
+  return (
+    <div className='containerImg'>
+      <img src={srcImg} alt={alt} />
+    </div>
+  )
+}
 
 export const Galeria = () => {
   return (
     <>
-    <div className='containerimg'>
-    <img src={imgFutbol1} alt="Futbol" />
-    <img src={imgrugby2} alt="Rugby" />
-    <img src={imgpaddle3} alt="Paddle" />
-    <img src={imgchicos4} alt="Kids" />
-    </div>
-    
-    
+      <section className='galeriaInicio'>
+        <ContenedorDeImg srcImg={imgFutbol1} alt={"Futbol"} />
+        <ContenedorDeImg srcImg={imgrugby2} alt={"Rugby"} />
+        <ContenedorDeImg srcImg={imgpaddle3} alt={"Paddle"} />
+        <ContenedorDeImg srcImg={imgchicos4} alt={"Kids"} />
+      </section>
     </>
   )
 }
