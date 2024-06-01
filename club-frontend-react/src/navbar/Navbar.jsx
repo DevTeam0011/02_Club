17bb79fc0a90f84376251e2461e6f2d5c5ffbaf4
Leandro Muro <vaxlemur@gmail.com>
2024-05-31 23:29:45 -0300

    Corregimos Navbar - Maquetacion

--- club-frontend-react/src/navbar/Navbar.jsx
+++ club-frontend-react/src/navbar/Navbar.jsx
@@ -1,44 +1,55 @@
-
+import logo from '../../public/imagenes/football-1275123_1280.jpg'
 
 export const Navbar = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-warning" >
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#"></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
+      <nav className="navbar navbar-expand-lg bg-warning" >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#"></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="animated slideInLeft"  ><img src={logo} alt="" className='logonavbar' /></li>
+              {/* <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Instalaciones</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Actividades
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Registrate</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-dark" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+        </li> */}
+              <li  className="nav-item animated slideInLeft " >
+                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+              </li>
+              
+                <li className="nav-item animated slideInLeft">
+                  <a className="nav-link" href="#">Instalaciones</a>
+                </li>
+                <li className="nav-item dropdown animated slideInLeft">
+                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Actividades
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Futbol</a></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><a className="dropdown-item" href="#">Rugby</a></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><a className="dropdown-item" href="#">Basquet</a></li>
+                    <li><hr className="dropdown-divider" /></li>
+                    <li><a className="dropdown-item" href="#">Natación</a></li>
+
+                    <li><hr className="dropdown-divider " /></li>
+                    <li><a className="dropdown-item" href="#">Voley</a></li>
+                  </ul>
+                </li>
+                
+            </ul>
+          
+
+
+            <button className="btn btn-outline-dark" type="submit">Iniciar Sesión</button>
+            <button className="btn btn-outline-dark" type="submit">Registrate</button>
+
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
